08e3eb8f589697278098e0c35867ac9cd3b4d1ca
akuldevali <akulg005@gmail.com>
2025-02-25 04:12:17 -0500

    Configured DB URL correctly

--- app/database.py
+++ app/database.py
@@ -4,7 +4,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+load_dotenv()
+
 SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
+if not SQLALCHEMY_DATABASE_URL:
+    raise ValueError("DATABASE_URL is not set!")
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
